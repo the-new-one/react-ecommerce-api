bec6f14b330b95468e99d7b6994aa1e88d010dcb
Lisandro Batiancila <BATIANCILA@MLHUILLIER.LOCAL>
2023-12-19 10:15:51 +0800

    demo2

--- src/firestore.module.ts
+++ src/firestore.module.ts
@@ -5,3 +5,23 @@ import {
   FirestoreOptionsProvider,
   FirestoreCollectionProviders,
 } from './firestore.providers';
+
+type FirestoreModuleOptions = {
+  imports: any[];
+  useFactory: (...args: any[]) => Settings;
+  inject: any[];
+};
+
+@Module({})
+export class FirestoreModule {
+  static forRoot(options: FirestoreModuleOptions): DynamicModule {
+
+    const optionsProvider = {
+      provide: FirestoreOptionsProvider,
+      useFactory: options.useFactory,
+      inject: options.inject,
+    };
+
+    return undefined;
+  }
+}
